410756a214992f80b7f45d2109e83ba265497378
EspiraMarvin <espiramarvin@gmail.com>
2021-01-05 18:04:43 +0300

    produc env

--- config/env/production.js
+++ config/env/production.js
@@ -51,7 +51,7 @@ module.exports = {
       // url: 'mysql://user:password@host:port/database',
 
       adapter: 'sails-postgresql',
-      url:'postgres://szolgexocuvkbi:2073feada3452600fd8b84ff39301af5c3d143e5b3fb3f8a0b1f8573c86af2aa@ec2-52-71-107-99.compute-1.amazonaws.com:5432/dbcsthv3g87l0k'
+      url:'postgres://szolgexocuvkbi:2073feada3452600fd8b84ff39301af5c3d143e5b3fb3f8a0b1f8573c86af2aa@ec2-52-71-107-99.compute-1.amazonaws.com:5432/dbcsthv3g87l0k',
 
       // url: 'mysql://user:password@host:port/database',
       // url: process.env.DATABASE_URL
@@ -80,7 +80,7 @@ module.exports = {
       * https://sailsjs.com/config/datastores                                     *
       *                                                                           *
       ****************************************************************************/
-      // ssl: true,
+      ssl: true,
 
     },
 
